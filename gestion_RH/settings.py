4c7677864c776bff0ffe9951a2045cd83bd6e62d
El adj Souleymane Nuhu <111816039+elsouleymane@users.noreply.github.com>
2025-04-10 17:01:48 +0000

    static

--- gestion_RH/settings.py
+++ gestion_RH/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +122,14 @@ STATICFILES_DIRS = [
     BASE_DIR / "gestion_RH" / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import os
+import dj_database_url
 
+# Utiliser la base de données PostgreSQL sur Render
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
